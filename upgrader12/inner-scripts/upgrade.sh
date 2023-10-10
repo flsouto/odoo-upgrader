639255ab63606fee1eea25ce5b5721a790c66213
flsouto <fabiolimasouto@gmail.com>
2023-10-10 01:15:48 -0300

    updates

--- upgrader12/inner-scripts/upgrade.sh
+++ upgrader12/inner-scripts/upgrade.sh
@@ -1,3 +1,4 @@
 echo '#!/usr/bin/env python3.7' > /OpenUpgrade/odoo-bin-tmp
 awk 'NR > 1' /OpenUpgrade/odoo-bin >> /OpenUpgrade/odoo-bin-tmp
+chmod 777 /OpenUpgrade/odoo-bin-tmp
 #todo run odoo-bin-tmp
